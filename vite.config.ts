048dfe643fb0c6bae9199fb3c0fdaf99d52d43de
John Lloyd <johnloyd.centeno@gmail.com>
2024-02-25 19:15:27 +0800

    add: github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "node:path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/os-schedulers/",
   resolve: {
     alias: {
       components: path.resolve(__dirname, "./src/components"),
